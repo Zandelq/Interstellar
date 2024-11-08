9453333caed0e3a81da53b394e212f083634f9e0
Zandelq <133067605+Zandelq@users.noreply.github.com>
2024-11-08 11:07:34 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   challenge: true, // Set to true if you want to enable password protection.
-  users: {
+  users: {Interstellar
     // You can add multiple users by doing username: 'password'.
     interstellar: 'password',
   },
